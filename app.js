08ca6dd72f0f39237ca3d990e6a2e4519d8e2e3d
Macheya Steele <mcheysteele@gmail.com>
2021-01-13 16:17:58 -0700

    modularized code

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const [name, github] = profileDataArgs;
 
 
 
 
+fs.writeFile(`index.html`, generatePage(name, github), err => {
+    if (err) throw new Error(err);
 
-
-const printProfileData = profileDataArr => {
-    //this
-    for (let i = 0; i < profileDataArr.length; i++) {
-    console.log(profileDataArr[i]);
-    }
-
-    console.log('================');
-
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => 
-      console.log(profileItem)
-    );
-};
-
-printProfileData(profileDataArgs);
+    console.log('Portfolio Complete! Check out html to see the output ');
+})
