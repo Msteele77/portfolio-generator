17eb832a7a84780ccb14b728e8369f80e61d833c
Macheya Steele <mcheysteele@gmail.com>
2021-01-15 10:05:42 -0700

    added question validations

--- app.js
+++ app.js
@@ -17,11 +17,27 @@ const inquirer = require('inquirer');
             type: 'input',
             name: 'name',
             message: 'What is your name?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name');
+                    return false;
+                }
+            }
           },
           {
             type: 'input',
             name: 'github',
             message: 'Enter your Github Username',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a GitHub Username');
+                    return false;
+                }
+            }
           },
           {
             type: 'input',
@@ -46,12 +62,28 @@ const inquirer = require('inquirer');
       {
         type: 'input',
         name: 'name',
-        message: 'What is the name of your project?'
+        message: 'What is the name of your project?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+             } else {
+                console.log('Please enter your project name');
+            return false;
+             }
+        }
       },
       {
         type: 'input',
         name: 'description',
-        message: 'Provide a description of the project (Required)'
+        message: 'Provide a description of the project (Required)',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('Please enter a project description');
+                return false;
+            }
+        }
       },
       {
         type: 'checkbox',
@@ -62,7 +94,15 @@ const inquirer = require('inquirer');
       {
         type: 'input',
         name: 'link',
-        message: 'Enter the GitHub link to your project. (Required)'
+        message: 'Enter the GitHub link to your project. (Required)',
+        validate: linkInput => {
+            if (linkInput) {
+                return true;
+            } else {
+                console.log('Please enter the link');
+                return false;
+            }
+        }
       },
       {
         type: 'confirm',
